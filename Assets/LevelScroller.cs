c6c1561c5afb92f631a976907fef65619fc6986b
phinixx14 <kendal.myers@hotmail.com>
2024-01-23 21:31:00 -0600

    basic level stuff

---
+++ Assets/LevelScroller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScroller : MonoBehaviour
+{
+    public Transform stage;
+    public float ScrollSpeed;
+    bool running = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running) {
+            Vector3 dir = Quaternion.AngleAxis(-20, Vector3.forward) * Vector3.down;
+            stage.Translate(dir * ScrollSpeed, Space.Self);
+        }
+    }
+
+    public void Stop() {
+        this.running = false;
+    }
+}
