057c73f2fb4506936c7d9c8dc79334de761ab773
phinixx14 <kendal.myers@hotmail.com>
2024-01-26 20:07:26 -0600

    WIP: working on behaviors

---
+++ Assets/_Game/Scripts/AbstractBehavior.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractBehavior
+{
+    public float duration;
+    public abstract void Act();
+}
